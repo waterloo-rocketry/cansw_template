f77ad09524ede23154efcff39fd95e2503000fab
BluCodeGH <22122986+BluCodeGH@users.noreply.github.com>
2021-12-22 19:47:24 -0500

    Fixes from running on hardware

--- board.h
+++ board.h
@@ -5,9 +5,9 @@
 
 #define MAX_LOOP_TIME_DIFF_ms 250
 
-#define RED_LED_PIN C2
-#define BLUE_LED_PIN C3
-#define WHITE_LED_PIN C4
+#define RED_LED_PIN C4
+#define BLUE_LED_PIN C2
+#define WHITE_LED_PIN C3
 #define LED_ON 0 // Whether the pin going high or low turns on the LED.
 
 void gpio_init(void);
